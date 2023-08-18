01913d52dd17fd6c248b02b97a8633142066d77f
Safronova Maryna <myjob.front.end@gmail.com>
2023-08-18 19:56:47 +0300

    first commit

---
+++ src/pages/auth/styles.ts
@@ -0,0 +1,20 @@
+import {makeStyles} from "@mui/styles";
+
+export const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+        padding: '20px',
+        backgroundColor: '#a3869d'
+    },
+    form: {
+        flex: 1,
+        
+    },
+    boxColor: {
+      backgroundColor: '#000000'
+    }
+})
